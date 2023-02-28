357586625c543b0951b027872701e97d1f981f70
DamienB325 <49534369+DamienB325@users.noreply.github.com>
2023-02-28 00:13:36 +0000

    BetterBread

---
+++ src/main/java/com/sustech/app/GameTextPrinter.java
@@ -0,0 +1,8 @@
+package com.sustech.app;
+import java.util.HashMap;
+
+public class GameTextPrinter {
+    public void printText(HashMap<> theHashMap) {
+        System.out.println(theHashMap.get(""));
+    }
+}
